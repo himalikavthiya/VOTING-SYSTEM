c88d0331050c1e183bcedfbf8a72c4b3297f647b
freni123 <frenivaghasiya@gmail.com>
2024-01-28 16:37:59 +0530

    election changed

--- voting-system/client/src/redux-saga/Admin-saga/create-election/reducer/electionReducer.js
+++ voting-system/client/src/redux-saga/Admin-saga/create-election/reducer/electionReducer.js
@@ -1,67 +1,66 @@
 import {
-    GET_ELECTION_FULLFILIED,
-    GET_ELECTION_PANDING,
-    GET_ELECTION_REJECTED,
-    POST_ELECTION_FULLFILIED,
-    POST_ELECTION_PANDING,
-    POST_ELECTION_REJECTED,
+  GET_ELECTION_FULLFILIED,
+  GET_ELECTION_PANDING,
+  GET_ELECTION_REJECTED,
+  POST_ELECTION_FULLFILIED,
+  POST_ELECTION_PANDING,
+  POST_ELECTION_REJECTED,
 } from "../action/action";
 
 const initialState = {
-    PartyData: [],
-    isLoading: false,
-    isError: null,
-}
+  electionData: [],
+  isLoading: false,
+  isError: null,
+};
 
 const electionReducer = (state = initialState, action) => {
-
-    switch (action.type) {
-        case GET_ELECTION_PANDING: {
-            return {
-                ...state,
-                isLoading: true,
-                isError: null,
-            };
-        }
-        case GET_ELECTION_FULLFILIED: {
-            return {
-                ...state,
-                isLoading: false,
-                PartyData: action.data,
-                isError: null,
-            };
-        }
-        case GET_ELECTION_REJECTED: {
-            return {
-                ...state,
-                isLoading: false,
-                isError: action.data,
-            };
-        }
-
-        case POST_ELECTION_PANDING:
-            return {
-                ...state,
-                isLoading: true,
-            };
-        case POST_ELECTION_FULLFILIED:
-            return {
-                ...state,
-                isError: action.data,
-            };
-        case POST_ELECTION_REJECTED:
-            return {
-                ...state,
-                isLoading: true,
-                    PartyData: state.PartyData.concat(action.data),
-                    isLoading: false,
-            };
-        default: {
-            return {
-                ...state,
-            }
-        }
+  switch (action.type) {
+    case GET_ELECTION_PANDING: {
+      return {
+        ...state,
+        isLoading: true,
+        isError: null,
+      };
     }
-}
+    case GET_ELECTION_FULLFILIED: {
+      return {
+        ...state,
+        isLoading: false,
+        electionData: action.data,
+        isError: null,
+      };
+    }
+    case GET_ELECTION_REJECTED: {
+      return {
+        ...state,
+        isLoading: false,
+        isError: action.data,
+      };
+    }
+//post
+    case POST_ELECTION_PANDING:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case POST_ELECTION_FULLFILIED:
+      return {
+        ...state,
+        isError: action.data,
+      };
+    case POST_ELECTION_REJECTED:
+      return {
+        ...state,
+        isLoading: true,
+        electionData: state.electionData.concat(action.data),
+        isLoading: false,
+      };
+    default: {
+      return {
+        ...state,
+      };
+    }
+  }
+};
 
 export default electionReducer;
